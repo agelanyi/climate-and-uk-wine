12eaacb5951fcb61d15c8140dc8fdb88ea959b0d
Ruby Johnson <74353542+rubyimogenjohnson@users.noreply.github.com>
2022-05-23 14:40:56 +0100

    Update script.js
    Adding full screen button.

--- website-frontend/js/script.js
+++ website-frontend/js/script.js
@@ -93,3 +93,40 @@ $("#fullpage").fullpage({
     }
   },
 });
+
+// Add screen toggle
+
+let myDocument = document.documentElement;
+let btn = document.getElementById("btn");
+btn.addEventListener("click", ()=>{
+    if(btn.textContent == "Click to Go Fullscreen"){
+        if (myDocument.requestFullscreen) {
+            myDocument.requestFullscreen();
+        } 
+        else if (myDocument.msRequestFullscreen) {
+            myDocument.msRequestFullscreen();
+        } 
+        else if (myDocument.mozRequestFullScreen) {
+            myDocument.mozRequestFullScreen();
+        }
+        else if(myDocument.webkitRequestFullscreen) {
+            myDocument.webkitRequestFullscreen();
+        }
+        btn.textContent = "Click to Exit Fullscreen";
+    }
+    else{
+        if(document.exitFullscreen) {
+            document.exitFullscreen();
+        }
+        else if(document.msexitFullscreen) {
+            document.msexitFullscreen();
+        }
+        else if(document.mozexitFullscreen) {
+            document.mozexitFullscreen();
+        }
+        else if(document.webkitexitFullscreen) {
+            document.webkitexitFullscreen();
+        }
+        btn.textContent = "Click to Go Fullscreen";
+    }
+});
